ec9a4c09c40e634c9729be4524f496385c0cfcd0
woaf <woaf@woaf-pc>
2018-12-20 18:12:42 +0100

    Initial commit.

---
+++ Reflection/src/reflection/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * The program uses jdbc:derby @ localhot:1527 with username woaf and psw 123
+ */
+package reflection.main;
+
+/**
+ *
+ * @author woaf
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
