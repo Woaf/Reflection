4ce9613e3c39e031cec77e6084c6035cdeda79e8
Woaf <bfazekas@inf.elte.hu>
2019-01-11 11:29:33 +0100

    Fixed imports. Extracted class information from entity stored in database.

--- Reflection/src/reflection/main/Main.java
+++ Reflection/src/reflection/main/Main.java
@@ -8,6 +8,10 @@
 package reflection.main;
 
 import base.Block;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -30,75 +34,54 @@ import javax.persistence.Query;
 public class Main {
 
     private static void fillDB() {
-        
+
         List<Object> objects = new ArrayList<>();
-        
+
         Block b = new Block(10, 20, 15, true, true);
         Block b2 = new Block(40, 20, 30, false, false);
-        
+
         objects.add(b);
         objects.add(b2);
-        
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ReflectionPU");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        
+
         for (Object object : objects) {
             em.persist(object);
         }
-        
+
         em.getTransaction().commit();
         em.close();
     }
-    
+
     private static String toCamelCase(String input) {
-        
+
         StringBuilder sb = new StringBuilder();
         sb.append(input.toLowerCase());
         sb.deleteCharAt(0);
         sb.insert(0, input.substring(0, 1).toUpperCase(), 0, 1);
         return sb.toString();
     }
-    
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        fillDB();
-        
-        List<String> tables = setupDatabaseConnection()
-                .stream()
-                .map(s -> toCamelCase(s))
-                .collect(Collectors.toList());
-        
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ReflectionPU");
-        EntityManager em = emf.createEntityManager();
-        Query q = em.createQuery("SELECT b FROM " + tables.get(0) + " b");
-        q.getResultList().forEach(System.out::println);
-        
-    }
 
     private static List<String> setupDatabaseConnection() {
-        
+
         Connection con;
         String driver = "org.apache.derby.jdbc.EmbeddedDriver";
         String dbName = "//localhost:1527/Reflection;user=woaf;password=123";
         String connectionURL = "jdbc:derby:" + dbName;
-        
+
         List<String> tableNameList = new ArrayList<>();
-        
+
         try {
             Class.forName(driver);
             con = DriverManager.getConnection(connectionURL);
             ResultSet sets = con.getMetaData().getTables(null, "WOAF", "%", null);
-            while(sets.next())
-            {
-                if(!sets.getString(3).equals("SEQUENCE"))
+            while (sets.next()) {
+                if (!sets.getString(3).equals("SEQUENCE")) {
                     tableNameList.add(sets.getString(3));
-                    //System.out.println(sets.getString(3));
+                }
+                //System.out.println(sets.getString(3));
             }
             con.close();
         } catch (SQLException | ClassNotFoundException ex) {
@@ -106,5 +89,58 @@ public class Main {
         }
         return tableNameList;
     }
-    
+
+    private static String createQueryString(String tableName) {
+        return "SELECT a FROM " + tableName + " a";
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        fillDB();
+
+        List<String> tables = setupDatabaseConnection()
+                .stream()
+                .map(s -> toCamelCase(s))
+                .collect(Collectors.toList());
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ReflectionPU");
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery(createQueryString(tables.get(0)));
+        q.getResultList().forEach(System.out::println);
+
+        tables.forEach((name) -> {
+            try {
+                Class<?> arbitraryClass = Class.forName("base." + name);
+                List<Constructor<?>> constructors = Arrays.asList(arbitraryClass.getDeclaredConstructors());
+
+                System.out.println(constructors.toString());
+
+                Object o = constructors.get(0).newInstance();
+
+                List<Field> oFields = Arrays.asList(o.getClass().getDeclaredFields());
+                List<Method> oMethods = Arrays.asList(o.getClass().getDeclaredMethods());
+
+                System.out.println("Fileds:");
+                oFields.forEach(System.out::println);
+                
+                System.out.println("Methods:");
+                oMethods.forEach(System.out::println);
+
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalArgumentException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InvocationTargetException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+    }
 }
